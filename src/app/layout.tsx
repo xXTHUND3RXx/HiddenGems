c79eebb0232089ec31b0f5dd370a7891b6c50e81
xXTHUND3RXx <fd192168@gmail.com>
2025-07-07 23:23:44 -0300

    :sparkles: feat: create component header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{backgroundColor: '#060706'}}  className={`flex w-full ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header
+      style={{ backgroundColor: "#090908" }}
+      className="w-full h-auto flex flex-col items-center font-(family-name:--roboto)"
+    >
+      <div className="flex items-center justify-betwenn gap-x-300">
+        <div>
+          <Image
+            src="/ChatGPT Image 7 de jul. de 2025, 22_36_43.png"
+            width={100}
+            height={100}
+            alt="safas"
+          ></Image>
+        </div>
+        <nav>
+          <ul style={{ color: "#EC021A" }} className="flex gap-4">
+            <button className="cursor-pointer">
+              <li>Games</li>
+            </button>
+            <button className="cursor-pointer">
+              <li>Generos</li>
+            </button>
+            <button className="cursor-pointer">
+              <li>Plataformas</li>
+            </button>
+          </ul>
+        </nav>
+      </div>
+      <div className="relative h-[500px] w-full flex flex-col items-center justify-center">
+        <div className="absolute inset-0 bg-[url('/hero-banner-bg.svg')] bg-cover bg-center opacity-40 z-0"></div>
+        <h1 className="z-10 text-[100px] font-bold text-white">Hidden Gems</h1>
+        <h2 className="z-11 text-2xl">Descubra sua proxima raridade indie</h2>
+      </div>
+    </header>
+  );
+}
